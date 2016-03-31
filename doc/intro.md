afb2edd258adc285b4c84149c85c94d4cbc87651
Jonathan Viccary <jonathan.viccary@gmail.com>
2016-03-30 20:58:33 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to keynes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/keynes/core.clj
@@ -0,0 +1,7 @@
+(ns keynes.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
